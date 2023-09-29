310fe4184833a160af1352e5be213218b1136a34
Rishabh Sharma <rish6501@gmail.com>
2023-09-29 13:45:29 +0530

    Added Response Entities in Movie Controller and Service
    Added Response Entities in Movie Controller and Service with exception handling

--- src/main/java/com/example/Moviereview/controller/movieController.java
+++ src/main/java/com/example/Moviereview/controller/movieController.java
@@ -4,9 +4,11 @@ import com.example.Moviereview.dtos.response.movieResponse;
 import com.example.Moviereview.entity.Genre;
 import com.example.Moviereview.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
@@ -16,22 +18,20 @@ public class movieController {
     @Autowired
     MovieService movieService;
 
-// add movie (give movieRequest) -done in admin Controller
-// search movie by title /:title
+
     @GetMapping("/id/{Id}")
-    public movieResponse getMovie(@PathVariable Long Id) {
+    public ResponseEntity<Object> getMovie(@PathVariable(name = "Id",required = true) Long Id) {
         return movieService.getMovieById(Id);
     }
 
-    @GetMapping("/title")
-    public movieResponse getMovie(@RequestParam String title) {
+    @GetMapping("/title/{movie-title}")
+    public ResponseEntity<Object> getMovie(@PathVariable(name = "movie-title",required = true) String title) {
         return movieService.getMovieFromTitle(title);
     }
 
-    public List<movieResponse> getMovieByGenre(@RequestParam Genre genre){
+
+    @GetMapping("/genre/{movie-genre}")
+    public ResponseEntity<List<movieResponse>> getMovieByGenre(@PathVariable(name = "movie-genre") String genre){
         return movieService.getMovieFromGenre(genre);
     }
-// update movie by Id /:id
-// delete movie by Id /:id
-//search sorted movie by rating using /:genre
 }
--- src/main/java/com/example/Moviereview/service/MovieService.java
+++ src/main/java/com/example/Moviereview/service/MovieService.java
@@ -8,6 +8,7 @@ import com.example.Moviereview.repository.MovieRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,41 +25,60 @@ public class MovieService {
     MovieRepository movieRepository;
 
 
-    public movieResponse getMovieFromTitle(String title) {
+    public static boolean isGenreValid(String genre){
+        try{
+            Genre.valueOf(genre);
+            return true;
+        }catch (IllegalArgumentException e)
+        {
+            return false;
+        }
+    }
+
+    public ResponseEntity<Object> getMovieFromTitle(String title) {
         logger.info("REST Request to Get Movie by title");
         try{
             Movie movie = movieRepository.findByTitle(title);
-            return movie.toMovieResponse();
+            return ResponseEntity.ok().body(movie.toMovieResponse());
         }
         catch (Exception e) {
+            e.printStackTrace();
             logger.error("No Movie Exist as a Title {} given",title);
         }
-        return null;
+        return ResponseEntity.badRequest().body("No Movie Exist as a Title : " + title);
     }
 
-    public movieResponse getMovieById(Long id) {
+    public ResponseEntity<Object> getMovieById(Long id) {
         logger.info("REST request to get Movie by Id");
         try{
             Optional<Movie> movie = movieRepository.findById(id);
-            return movie.get().toMovieResponse();
+            return ResponseEntity.ok().body(movie.get().toMovieResponse());
         }
         catch (Exception e){
+            e.printStackTrace();
             logger.error("No such Movie Exist by Id {}",id);
         }
-        return null;
+        return ResponseEntity.badRequest().body("No Such Movie exist of Id : " + id);
     }
 
-    public List<movieResponse> getMovieFromGenre(Genre genre) {
-        logger.info("REST request to get Movie by genre!");
+    public ResponseEntity<List<movieResponse>> getMovieFromGenre(String genre) {
+        logger.info("REST request to get Movie by genre : {} ",genre);
 
-        List<Movie> movies = movieRepository.findByGenre(genre);
+        // check first if genre exist in enum
+        if(!isGenreValid(genre))
+        {
+            logger.info("No Genre Exist of type {}",genre);
+            return ResponseEntity.badRequest().body(new ArrayList<>());
+        }
+
+        List<Movie> movies = movieRepository.findByGenre(Genre.valueOf(genre));
         if(movies.size() == 0)
-            return new ArrayList<>();
+            return ResponseEntity.ok().body(new ArrayList<>());
         List<movieResponse> ans = new ArrayList<>();
 
         for (Movie movie : movies) {
             ans.add(movie.toMovieResponse());
         }
-        return ans;
+        return ResponseEntity.ok().body(ans);
     }
 }
